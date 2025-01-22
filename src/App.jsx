be6f015a8aff5a434873d92cf3e6fd04d284016e
alexxxey <aleksejs.gorlovics@gmail.com>
2025-01-22 13:35:53 +0200

    added an element that displays current window size and updates when window is resized

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,24 @@ import alisa_picture from './assets/alisa_picture.jpg';
 import brendon_picture from './assets/brendon_picture.jpg';
 import react_logo from './assets/react.svg';
 
+import React, {useState, useEffect} from 'react';
+
 function App() {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    function handleWindowResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+    }, []);
+
     return (
-        <>
+        <main className='my-8'>
             <div className='flex flex-col gap-y-2 items-center mt-8 mb-8'>
                 <img className='w-[15%]' src={react_logo} alt="React Logo" />
                 <h1 className='text-center font-bold text-6xl text-[#61DBFB]'>REACT DEMO</h1>
@@ -21,17 +36,23 @@ function App() {
             
             <div className='w-full h-[30px] bg-[#61DBFB]'></div>
 
-            <div className='flex flex-row gap-x-24 justify-center mt-8 mb-8'>
-                <div className='flex flex-col gap-y-8'>
-                    <h1 className='text-4xl font-bold text-center'>Color picker</h1>
-                    <ColorPicker></ColorPicker>
+            <div className='flex flex-col items-center gap-y-4'>
+                <div className='flex flex-row gap-x-24 justify-center mt-8 mb-8'>
+                    <div className='flex flex-col gap-y-8'>
+                        <h1 className='text-4xl font-bold text-center'>Color picker</h1>
+                        <ColorPicker></ColorPicker>
+                    </div>
+                    <div className='flex flex-col gap-y-8'>
+                        <h1 className='text-4xl font-bold text-center'>To-Do list</h1>
+                        <ToDoList></ToDoList>
+                    </div>
                 </div>
-                <div className='flex flex-col gap-y-8'>
-                    <h1 className='text-4xl font-bold text-center'>To-Do list</h1>
-                    <ToDoList></ToDoList>
+
+                <div>
+                    <p className='text-xl'><span className='font-bold'>Window size:</span> {width}px x {height}px</p>
                 </div>
             </div>
-        </>
+        </main>
     );
 }
 
